cff72e3a2ffbfa763f4df4283c651c205842f04d
Jed Peek <jedpeek@yahoo.com>
2018-06-30 17:59:52 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,5 +1,5 @@
 class Pokemon
-  db = SQLite3::Database.new('db/pokemon.db').execute('db/schema_migration.sql')
+  db = SQLite3::Database.new('db/pokemon.db')
   @@all = []
   attr_accessor :name, :type
 
